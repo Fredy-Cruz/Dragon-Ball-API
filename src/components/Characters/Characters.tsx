d010a5a112b599ee09ce0797f0d171f9787aa8d0
Fredy-Cruz <fredyjcr0601@gmail.com>
2024-12-17 17:41:11 -0600

    Add Pagination and use SWR

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -1,17 +1,45 @@
 import style from './Characters.module.css';
-import { useEffect } from 'react';
+/* import { useEffect } from 'react';*/
 import { useState } from 'react';
 import { Loading } from '../Loading/Loading';
 import { ErrorIcon } from '../ErrorIcon/ErrorIcon';
-import { Like } from '../Like/Like';
+/* import { Like } from '../Like/Like'; */
 import { getCharacters } from '@services/characters.services';
-import { Item } from '@interfaces/characters.interfaces';
-
-
+/* import { Item } from '@interfaces/characters.interfaces'; */
+import useSWR from 'swr';
 
 export const Characters = () => {
 
-    const [characters, setCharacters] = useState<Item[]>([]);
+    //Creando un UseState para el cambio de pagina
+    const[page, setPage] = useState<number>(1)
+
+    //SWR no permite enviar parametros, asi que hacemos esto
+    const fetcher = () => getCharacters(page,10);
+
+    //Utilizando SWR para manejar la data de la API
+    const {data:characters, error, isLoading} = useSWR(`api/characters?page=${page}`, fetcher)
+    //data: characters  es un alias
+
+    //Cambio de Pagina
+    const nextPage = () =>{
+        if(page == 6){
+            return setPage(page);
+        }
+        else{
+            setPage(page+1);
+        }
+    }
+
+    const PreviousPage = () =>{
+        if(page == 1){
+            return setPage(page);
+        }
+        else{
+            setPage(page-1);
+        }
+    }
+
+    /* const [characters, setCharacters] = useState<Item[]>([]);
     const [error, setError] = useState<string | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
 
@@ -28,19 +56,19 @@ export const Characters = () => {
                 setLoading(false);
             }
         })()
-    }, [])
+    }, []) */
 
     return (
-        <div>
+        <div className={style.main}>
             <h2 className={style.sub_title}>Dragon Ball Characters</h2>
             {
                 error && <div className={style.div_load}><ErrorIcon /></div>
             }
             {
-                loading && <div className={style.div_load}><Loading /></div>
+                isLoading && <div className={style.div_load}><Loading /></div>
             }
             <div className={style.cont}>
-                {characters.map((character) => (
+                {characters?.map((character) => (
                     //Cada personaje debe tener un atributo unico, por eso usamos key
                     <div key={character.id} className={style.container}>
                         <figure>
@@ -51,12 +79,18 @@ export const Characters = () => {
                             <h3>Gender: {character.gender}</h3>
                             <h3>Ki: {character.ki}</h3>
                         </div>
-                        <div className={style.footer}>
+                        {/* <div className={style.footer}>
                             <Like />
-                        </div>
+                        </div> */}
                     </div>
 
                 ))}
+            </div>
+
+            <div className={style.pagination}>
+                <button onClick={PreviousPage} className={style.paginationButton}>Previous</button>
+                <span className={style.paginationText}>Page {page} / 6</span>
+                <button onClick={nextPage} className={style.paginationButton}>Next</button>
             </div>
         </div>
     )
--- src/services/characters.services.tsx
+++ src/services/characters.services.tsx
@@ -1,7 +1,7 @@
 import { ICharacters } from "@interfaces/characters.interfaces";
 
-export const getCharacters = async () => {
-    const response = await fetch('https://dragonball-api.com/api/characters');
+export const getCharacters = async (page:number, limit:number) => {
+    const response = await fetch(`https://dragonball-api.com/api/characters?page=${page}&limit=${limit}`);
     if (!response.ok) {
         throw new Error('Peticion API fallida');
     }
